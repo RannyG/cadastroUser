efe2c305eb39ddd78dfb1e1e5202b21812555055
Ranny Gabriel <rannygabrizao12@gmail.com>
2025-11-05 15:51:13 -0400

    finalização do CRUD

--- cadastro-usuario/src/main/java/com/treino/cadastro_usuario/business/UsuarioService.java
+++ cadastro-usuario/src/main/java/com/treino/cadastro_usuario/business/UsuarioService.java
@@ -28,4 +28,16 @@ public class UsuarioService {
     public void deletarUsuarioPorEmail(String email){
         repository.deleteByEmail(email);
     }
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario){
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(()->
+            new RuntimeException("Usuario não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+            .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+            .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getEmail())
+            .id(usuarioEntity.getId())
+            .build();
+
+        repository.saveAndFlush(usuarioAtualizado);
+    }
 }
